b2b2678c45b8d479af31b94e5af9cb0887841d8e
afmurillo97 <afmurillo97@gmail.com>
2022-03-19 16:14:24 -0500

    four excercise is done!!!

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let result = 0;
+    if ((a < 0 || b < 0) || (typeof a !== 'number' || typeof b !== 'number')) {
+        return 'ERROR';
+    }
+    else if (a < b){
+        for (let i = 1; i <= b; i++) {
+            result += i
+        }
+    } else {
+        for (let i = 1; i <= a; i++) {
+            result += i
+        }
+    }
+    return result;
 };
 
 // Do not edit below this line
