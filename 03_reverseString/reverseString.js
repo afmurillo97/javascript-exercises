b66512b14ef05bb4dcbc164fc3052f43e4154a33
afmurillo97 <afmurillo97@gmail.com>
2022-03-19 13:42:43 -0500

    second excercise is done!!!

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let arrayString = [];
+    for (let i = 0; i < string.length; i++) {
+        arrayString.push(string.slice(i, i + 1))
+    }
+    let reverseString = arrayString.reverse();
+    let result = reverseString.join('')
+    return result;
 };
 
 // Do not edit below this line
