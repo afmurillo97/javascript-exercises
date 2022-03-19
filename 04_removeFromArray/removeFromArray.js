0295e71c9e4f93f5d9765adf855a23e33d04f5a2
afmurillo97 <afmurillo97@gmail.com>
2022-03-19 15:46:25 -0500

    third excercise is done!!!

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,29 @@
 const removeFromArray = function() {
-
+    let evaluatedArray = [];
+    let parameters = [];
+    for (let i = 0; i < arguments.length; i++) {
+        if (i == 0){
+            evaluatedArray = arguments[i];
+        } else {
+            parameters.push(arguments[i]);
+        }
+    }
+    let cont  = parameters.length
+    while (cont > 0){
+        for (let i = 0; i < evaluatedArray.length; i++) {
+            if (evaluatedArray[i] === parameters[cont-1]){
+                evaluatedArray.splice(i,1)
+            }
+        }
+    cont--;
+    }
+    return evaluatedArray;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
+//removeFromArray([1, 2, 3, 4], 3)
+
+//toEqual([1, 4])
