28b3a5c8b64e0a3f62e1ce0e4de958ffee5c6eb7
afmurillo97 <afmurillo97@gmail.com>
2022-03-19 17:53:27 -0500

    sixth excercise is done!!!

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+  let result = parseFloat((fahrenheit - 32) * 5/9);
+  let number = parseFloat(result.toFixed(1));
+  return number;
 };
 
-const ctof = function() {
-
+const ctof = function(celcius) {
+  let result = parseFloat(celcius * 9/5 + 32);
+  let number = parseFloat(result.toFixed(1));
+  return number;
 };
 
 // Do not edit below this line
