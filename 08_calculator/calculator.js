33852e0afc435cf7278cf8dfb8839d243158eea2
afmurillo97 <afmurillo97@gmail.com>
2022-03-19 19:03:59 -0500

    seventh excercise is done!!!

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,47 @@
-const add = function() {
-	
+const add = function(a, b) {
+	let result = a + b;
+  return result;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	let result = a - b;
+  return result;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let result = 0;
+  for (let i = 0; i < array.length; i++) {
+      result += array[i];
+    }
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+	let result = 1;
+  for (let i = 0; i < array.length; i++) {
+      result *= array[i];
+    }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	let result = a;
+  for (let i = 0; i < b-1; i++) {
+    result *= a;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let result = 1;
+  let cont = number;
+  if (number === 0){
+    return 1;
+  }
+  for (let i = cont; i > 1 ; i--) {
+    result *= i-1
+  }
+  return result * number;
 };
 
 // Do not edit below this line
