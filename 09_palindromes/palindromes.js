9e8d0cbab3963c878effd09012199b15825569e7
afmurillo97 <afmurillo97@gmail.com>
2022-03-20 11:58:53 -0500

    eighth excercise is done!!!

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,22 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    // delete from string all specials characters, '\b'  clear the white space
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions/Cheatsheet 
+    let clearString = (string.replace(/[^\ba-zA-Z]/g, ''));
+    let result = clearString.toLowerCase();
+
+    let arrayString = [];
+    for (let i = 0; i < result.length; i++) {
+        arrayString.push(result.slice(i, i + 1));
+    }
+    let reverseString = arrayString.reverse()
+    let reverse = reverseString.join('');
+    
+    if (reverse === result){
+        return true;
+    } else {
+        return false;
+    }
+
 
 };
 
