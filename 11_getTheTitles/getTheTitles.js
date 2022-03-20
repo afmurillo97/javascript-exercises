626deaaa303d3bec9f77f7c4586a5b603fa0cf62
afmurillo97 <afmurillo97@gmail.com>
2022-03-20 14:22:33 -0500

    getTheTitles is done!!

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,15 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(object) {
+    let result = [];
+    for (let i = 0; i < object.length; i++) {
+        result.push(object[i].title);
+    };
+    return result;
+    // return the authors of all books
+    // let result = [];
+    // for (let i = 0; i < author.length; i++) {
+    //     result.push(object[i].author);
+    // }
+    // return result;
 };
 
 // Do not edit below this line
