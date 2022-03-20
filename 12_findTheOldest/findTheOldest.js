c4c3401b176d0cbffc9992be71d53f6fde83ba0f
afmurillo97 <afmurillo97@gmail.com>
2022-03-20 16:37:36 -0500

    findTheOldest is done!!!

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(object) {
+    let result = 0;
+    let major = 0;
+    let res = object[0];
+    let live = '';
+    const d = new Date();
+     for (let i = 0; i < object.length; i++) {
+        live = typeof object[i].yearOfDeath
+        if (live === 'undefined'){
+            result = d.getFullYear() - object[i].yearOfBirth
+            if (major < result){
+                major = result;
+                res = object[i]
+            }
+        }
+        result = object[i].yearOfDeath - object[i].yearOfBirth
+        if (major < result){
+            major = result;
+            res = object[i]
+        }
+    }
+    return res
 };
 
 // Do not edit below this line
