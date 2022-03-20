53dd7307aa6088fc67be82954d9f7ef7a2318943
afmurillo97 <afmurillo97@gmail.com>
2022-03-20 13:20:04 -0500

    nineth excercise is done!!!

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    let f1 = 1;
+    let f2 = 1;
+    let result = 0;
+    if (n < 1 ){
+        return 'OOPS';
+    }
+    if (n == 1 || n == 2){
+        result = 1;
+        return result;
+    }
+    for (let i = 2; i < n; i++) {
+        result = f1 + f2;
+        f1 = f2;
+        f2 = result
+    }
 
+    return result
 };
 
 // Do not edit below this line
